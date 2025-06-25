44669410dfe7bf952a9a490e359b4c3b104111a4
Khushi Gupta <112020768+uniquekhushi@users.noreply.github.com>
2025-06-25 14:16:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+//import MainContainer from "./Components/MainContainer";
+import Login from "./Components/Login";
+
+function App() {
+  return <div className = "App">
+    <Login />
+    {/*<MainContainer />*/}
+    </div>;
+  
+}
+
+export default App;
